de6fde35a4f89ed9a59e03ca23fc035784064ce8
9547 <nivefive9547@gmail.com>
2021-06-29 23:11:00 +0800

    docker: use debian:buster-slim as node base image

--- docker/control/init.sh
+++ docker/control/init.sh
@@ -5,19 +5,19 @@
 
 if [ ! -f ~/.ssh/known_hosts ]; then
     mkdir -m 700 ~/.ssh
-    echo $SSH_PRIVATE_KEY | perl -p -e 's/↩/\n/g' > ~/.ssh/id_rsa
+    echo "$SSH_PRIVATE_KEY" | perl -p -e 's/↩/\n/g' > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
-    echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
+    echo "$SSH_PUBLIC_KEY" > ~/.ssh/id_rsa.pub
     echo > ~/.ssh/known_hosts
     for f in $(seq 1 5);do
-	ssh-keyscan -t rsa n$f >> ~/.ssh/known_hosts
+        ssh-keyscan -t rsa n"$f" >> ~/.ssh/known_hosts
     done
 fi
 
 
 # TODO: assert that SSH_PRIVATE_KEY==~/.ssh/id_rsa
 
-cat <<EOF 
+cat <<EOF
 Welcome to tiup-cluster on Docker
 ===========================
 
